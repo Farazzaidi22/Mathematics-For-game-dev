36a1007c671af5aa7ed6c3320f33896f75a01495
Faraz Khalid <farazkhalid05@yahoo.com>
2020-04-16 00:26:30 +0500

    Third commit

---
+++ Vector basics/Assets/Scripts/Drive.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+// A very simplistic car driving on the x-z plane.
+
+public class Drive : MonoBehaviour
+{
+    //Vector2 up = new Vector2(0, 0.1f);
+    //Vector2 right = new Vector2(0.1f, 0);
+
+    public GameObject fuel;
+    float speed = 5f;
+    Vector3 direction;
+    float stopPoint = 0.1f;
+
+    private void Start()
+    {
+        direction = fuel.transform.position - this.transform.position;
+        Coord dir = MyMath.GetNormal(new Coord(direction));
+        direction = dir.ToVector();
+    }
+
+    void Update()
+    {
+        //if(Vector3.Distance(this.transform.position, fuel.transform.position) > stopPoint)
+        if(MyMath.GetDistance(new Coord(this.transform.position), new Coord(fuel.transform.position)) > stopPoint)
+        {
+            this.transform.position += direction * speed * Time.deltaTime;
+        }
+        
+        
+        //Vector3 position = this.transform.position;
+
+        //if(Input.GetKey(KeyCode.UpArrow))
+        //{
+        //    position.x += up.x;
+        //    position.y += up.y;
+        //}
+
+        //else if (Input.GetKey(KeyCode.DownArrow))
+        //{
+        //    position.x += up.x;
+        //    position.y += - up.y;
+        //}
+
+        //else if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    position.x += right.x;
+        //    position.y += right.y;
+        //}
+
+        //else if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    position.x += - right.x;
+        //    position.y += right.y;
+        //}
+
+        //this.transform.position = position;
+    }
+}
---
+++ Vector basics/Assets/Scripts/MyMath.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyMath
+{
+    static public Coord GetNormal(Coord vector)
+    {
+        float length = GetDistance(new Coord(0, 0, 0), vector);
+        vector.x /= length;
+        vector.y /= length;
+        vector.z /= length;
+
+        return vector;
+    }
+
+    static public float GetDistance(Coord point1, Coord point2)
+    {
+        float diffSquared = Mathf.Sqrt(Square(point1.x - point2.x) +
+                                       Square(point1.y - point2.y) +
+                                       Square(point1.z - point2.z));
+        return diffSquared;
+    }
+
+    static public float Square(float value)
+    {
+        return value * value;
+    }
+}
